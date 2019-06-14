b84523126e28ce69e7cb84fbfefd60f002b468c5
BETeng <brian7.teng@gmail.com>
2019-06-14 00:09:44 -0500

    made routes connecting pages

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,29 @@
+var friendData = require("../data/friends");
+
+module.exports = function(app){
+  app.get("/api/friends", function(req, res){
+    res.json(friendData);
+    console.log("called friendData apiRoute");
+  });
+  
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// app.post("/api/friends", function(req, res){
+//   var newFriend = req.body;
+//   console.log(newFriend);
+//   friendData.push(newFriend);
+//   res.json(newCharacter);
+// })
--- server.js
+++ server.js
@@ -2,11 +2,14 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
 app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT);
+  console.log("App listening on http://localhost:" + PORT);
 });
