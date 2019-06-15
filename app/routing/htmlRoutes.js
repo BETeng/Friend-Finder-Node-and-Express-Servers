53695de7d83348761c9cb193b4d584c8eb10bdeb
BETeng <brian7.teng@gmail.com>
2019-06-15 18:37:54 -0500

    set up html routes

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -5,4 +5,8 @@ module.exports = function(app){
     res.sendFile(path.join(__dirname, "../public/home.html"));
     console.log("homepage called")
   });
+  app.get("/survey", function(req, res){
+    res.sendFile(path.join(__dirname, "../public/survey.html"));
+    console.log("survey html called")
+  });
 };
