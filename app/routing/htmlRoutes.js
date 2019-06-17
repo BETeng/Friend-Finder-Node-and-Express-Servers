42778c81c19f9b197f574020a47c2f22a8b022b0
BETeng <brian7.teng@gmail.com>
2019-06-17 10:44:13 -0500

    set up object

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,12 +1,12 @@
 const path = require("path");
 
 module.exports = function(app){
-  app.get("/", function(req, res){
-    res.sendFile(path.join(__dirname, "../public/home.html"));
-    console.log("homepage called")
-  });
   app.get("/survey", function(req, res){
     res.sendFile(path.join(__dirname, "../public/survey.html"));
     console.log("survey html called")
+  });
+  app.get("*", function(req, res){
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+    console.log("homepage called")
   });
 };
