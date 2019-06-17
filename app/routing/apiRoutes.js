17220307e92f83cef25567bc1e67a546d2d478e7
BETeng <brian7.teng@gmail.com>
2019-06-17 18:24:10 -0500

    got the modal to appear and array to appear as int

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,25 +5,19 @@ module.exports = function(app){
     res.json(friendData);
     console.log("called friendData apiRoute");
   });
-  
-};
+ app.post("/api/friends", function(req, res){
+  var newFriend = req.body;
+  newFriend.scores = newFriend.scores.map(el=>parseInt(el));
+  //console.log(newFriend);
+  friendData.push(newFriend);
+  console.log(newFriend.scores);
+  res.json(friendData);
 
+})
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-// app.post("/api/friends", function(req, res){
-//   var newFriend = req.body;
-//   console.log(newFriend);
-//   friendData.push(newFriend);
-//   res.json(newCharacter);
-// })
+// for (var i=0; i<friendData.length; i++){
+//   totalDifference = Math.abs(friendData[i].scores - newFriend.scores)
+//   // still need to add logic setup for subtracitng arrays 
+//  // not sure if need to change values in array to numbers first or if the math logic will do it
+// }
